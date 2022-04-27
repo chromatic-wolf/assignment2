647db7f92b7cf13e0eb289e20b8a52862c76dc64
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2022-04-27 17:39:03 +1000

    Create Test.java
    Added test class

---
+++ src/assignment2/Test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment2;
+
+/**
+ *
+ * @author caleb
+ */
+public class Test {
+    private int TestID;
+    private int PatientID;
+    private String testType;
+    private boolean testResult;
+}
